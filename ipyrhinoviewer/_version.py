d80fb72d8fbbeefb09dcca8dd68e41c578b30dc5
Jäger, Florian <florian.jaeger@tuwien.ac.at>
2022-08-08 16:39:05 +0200

    Updated version

--- ipyrhinoviewer/_version.py
+++ ipyrhinoviewer/_version.py
@@ -4,5 +4,5 @@
 # Copyright (c) Florian Jaeger.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 2, 3)
+version_info = (0, 2, 4, 'dev')
 __version__ = ".".join(map(str, version_info))
