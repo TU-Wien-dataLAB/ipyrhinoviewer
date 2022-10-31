92a8f7509b798861e783608d980d40cdea35b5ce
Jäger, Florian <florian.jaeger@tuwien.ac.at>
2022-10-31 11:04:15 +0100

    Bump to 0.2.5

--- ipyrhinoviewer/_version.py
+++ ipyrhinoviewer/_version.py
@@ -4,5 +4,5 @@
 # Copyright (c) Florian Jaeger.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 2, 5, 'dev')
+version_info = (0, 2, 5)
 __version__ = ".".join(map(str, version_info))
