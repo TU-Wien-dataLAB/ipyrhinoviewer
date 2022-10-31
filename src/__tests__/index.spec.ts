5a87d28b9358bae13d6101efadfcb197168bdfad
Jäger, Florian <florian.jaeger@tuwien.ac.at>
2022-10-31 12:53:19 +0100

    Added new test cases

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -6,21 +6,82 @@
 
 import { createTestModel } from './utils';
 
-import { RhinoModel } from '..';
+import { resolveUrl, RhinoModel } from '..';
+
+describe('test resolve url', () => {
+  it('correct absolute path in jupyterlab', () => {
+    const path = '/test/test.3dm';
+    window = Object.create(window);
+    const url =
+      'http://localhost:8080/user/j106930/lab/tree/test/Introduction%20to%20Python.ipynb';
+    Object.defineProperty(window, 'location', {
+      value: {
+        href: url,
+      },
+    });
+
+    expect(resolveUrl(path)).toEqual(
+      'http://localhost:8080/user/j106930/tree/test/test.3dm'
+    );
+  });
+  it('correct absolute path in nbclassic', () => {
+    const path = '/test/test.3dm';
+    window = Object.create(window);
+    const url =
+      'http://localhost:8080/notebooks/test/Introduction%20to%20Python.ipynb';
+    Object.defineProperty(window, 'location', {
+      value: {
+        href: url,
+      },
+    });
+    expect(resolveUrl(path)).toEqual(
+      'http://localhost:8080/tree/test/test.3dm'
+    );
+  });
+  it('correct relative path in jupyterlab', () => {
+    const path = 'test.3dm';
+    window = Object.create(window);
+    const url =
+      'http://localhost:8080/user/j106930/lab/tree/test/Introduction%20to%20Python.ipynb';
+    Object.defineProperty(window, 'location', {
+      value: {
+        href: url,
+      },
+    });
+
+    expect(resolveUrl(path)).toEqual(
+      'http://localhost:8080/user/j106930/tree/test/test.3dm'
+    );
+  });
+  it('correct relative path in nbclassic', () => {
+    const path = 'test.3dm';
+    window = Object.create(window);
+    const url =
+      'http://localhost:8080/notebooks/test/Introduction%20to%20Python.ipynb';
+    Object.defineProperty(window, 'location', {
+      value: {
+        href: url,
+      },
+    });
+    expect(resolveUrl(path)).toEqual(
+      'http://localhost:8080/tree/test/test.3dm'
+    );
+  });
+});
 
 describe('Rhino', () => {
   describe('ExampleModel', () => {
     it('should be creatable', () => {
       const model = createTestModel(RhinoModel);
       expect(model).toBeInstanceOf(RhinoModel);
-      expect(model.get('value')).toEqual('Hello World');
+      expect(model.get('path')).toEqual('');
     });
 
     it('should be creatable with a value', () => {
-      const state = { value: 'Foo Bar!' };
+      const state = { path: '/test.3dm' };
       const model = createTestModel(RhinoModel, state);
       expect(model).toBeInstanceOf(RhinoModel);
-      expect(model.get('value')).toEqual('Foo Bar!');
+      expect(model.get('path')).toEqual('/test.3dm');
     });
   });
 });
